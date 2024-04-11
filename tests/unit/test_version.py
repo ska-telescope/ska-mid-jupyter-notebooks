c557c4b899816c6b75d9c623e512b4dfd7e6430f
Pieter Jordaan <gitlab@jordaan.me>
2024-04-11 13:44:46 +0200

    AT-1954 - Fix name of src package.

--- tests/unit/test_version.py
+++ tests/unit/test_version.py
@@ -1,8 +1,8 @@
 """This module tests the ska_ser_test_equipment version."""
 
-import ska_mid_jupyter_scripting
+import ska_mid_jupyter_notebooks
 
 
 def test_version() -> None:
-    """Test that the ska_mid_jupyter_scripting version is as expected."""
-    assert ska_mid_jupyter_scripting.__version__ == "0.1.0"
+    """Test that the ska_mid_jupyter_notebooks version is as expected."""
+    assert ska_mid_jupyter_notebooks.__version__ == "0.1.0"
