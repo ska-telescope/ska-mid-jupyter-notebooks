2a335c48a0a689f268c020e837ea0f3486d4b5b7
Nkosingphile Nzotho <nnzotho@sarao.ac.za>
2024-05-10 08:28:22 +0200

    AT-1952 Added telescope to operate mode method

--- src/ska_mid_jupyter_notebooks/dish/dish.py
+++ src/ska_mid_jupyter_notebooks/dish/dish.py
@@ -29,6 +29,10 @@ class DishManager(DishDeviceProxy):
     @property
     def dish_mode(self) -> DishMode:
         return DishMode(self._device_proxy.dishMode)
+    
+    @dish_mode.setter
+    def x(self, value):
+        self._device_proxy.dishMode = value
 
     @property
     def power_state(self) -> PowerState:
@@ -88,6 +92,11 @@ class TangoDishDeployment(TangoDeployment):
     @property
     def spfrx_in_the_loop(self) -> bool:
         return f"{self.dish_id}/spfrxpu/controller" in self.devices
+
+    def set_dish_operational(self):
+        dish_manager =  self.dish_manager
+        print(f"{self.dish_id}: setting to operate mode")
+        dish_manager.SetOperateMode()
 
     def reset_dish(self):
         dish_manager = self.dish_manager
