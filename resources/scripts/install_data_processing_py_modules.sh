4b6e602df79a917f8790856d9bda1abc449cc4cb
nnzotho <nnzotho@sarao.ac.za>
2024-08-11 21:28:44 +0200

    AT-2225 Added script to install additional python modules

---
+++ resources/scripts/install_data_processing_py_modules.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Downgrade protobuf to 3.20.1
+sed -i '/\[tool.poetry.dependencies\]/a protobuf = "3.20.1"' ../../pyproject.toml
+#update the lock file
+cd ../../ && poetry lock --no-update && poetry install
+
+#Prepare to install casadata module.
+#The following directory structure is needed for casadata to work.
+#It is the download location of the casadata
+mkdir -p ~/.casa && mkdir -p ~/.casa/data
+
+pip install casadata && pip install casatasks && pip install casaviewer
+
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
