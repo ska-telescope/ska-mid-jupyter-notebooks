330b2c7fb24896beb94f1cdf219efd2601018fde
nnzotho <nnzotho@sarao.ac.za>
2024-08-13 10:28:19 +0200

    AT-2225 Applied all suggestions from MR

--- resources/scripts/install_data_processing_py_modules.sh
+++ resources/scripts/install_data_processing_py_modules.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-#Downgrade protobuf to 3.20.1
-sed -i '/\[tool.poetry.dependencies\]/a protobuf = "3.20.1"' ../../pyproject.toml
-#update the lock file
-cd ../../ && poetry lock --no-update && poetry install
+cd ../../
 
 #Prepare to install casadata module.
 #The following directory structure is needed for casadata to work.
 #It is the download location of the casadata
-mkdir -p ~/.casa && mkdir -p ~/.casa/data
+mkdir -p ~/.casa/data
 
-pip install casadata && pip install casatasks && pip install casaviewer
+#Add required python libraries
+poetry add protobuf=="3.20.1" 
+poetry add casadata 
+poetry add casatasks
+poetry add casaviewer
+
+#update the lock file
+poetry lock --no-update && poetry install
 
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
-echo "*** Casa modules installed with success***"
+echo "*** Casa modules installed with success. ***"
